7a2a9861eebf3ecc6372c3a85968ac9f254af8d9
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-11-03 11:11:43 -0400

    started repo

---
+++ src/start-server-and-test-spec.js
@@ -0,0 +1,10 @@
+'use strict'
+
+/* eslint-env mocha */
+const startServerAndTest = require('.')
+
+describe('start-server-and-test', () => {
+  it('write this test', () => {
+    console.assert(startServerAndTest, 'should export something')
+  })
+})
