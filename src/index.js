e3b8dd651296fcfbc899646680636f8b0a47fdf5
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-11-07 14:17:03 +0100

    feat: working wait

--- src/index.js
+++ src/index.js
@@ -2,12 +2,53 @@
 
 const la = require('lazy-ass')
 const is = require('check-more-types')
+const execa = require('execa')
+const waitOn = require('wait-on')
+const Promise = require('bluebird')
+const debug = require('debug')('start-server-and-test')
 
 function startAndTest ({ start, url, test }) {
   la(is.unemptyString(start), 'missing start script name', start)
   la(is.unemptyString(test), 'missing test script name', test)
   la(is.url(url), 'missing url to wait on', url)
-  return Promise.resolve()
+
+  debug('starting server')
+  const server = execa('npm', ['run', start], { stdio: 'inherit' })
+  let serverStopped
+
+  function stopServer () {
+    if (!serverStopped) {
+      debug('stopping server')
+      server.kill()
+      serverStopped = true
+    }
+  }
+
+  const waited = new Promise((resolve, reject) => {
+    waitOn(
+      {
+        resources: [url]
+      },
+      err => {
+        if (err) {
+          debug('error waiting for url', url)
+          debug(err.message)
+          return reject(err)
+        }
+        resolve()
+      }
+    )
+  })
+
+  function runTests () {
+    debug('running test script command', test)
+    return execa('npm', ['run', test], { stdio: 'inherit' })
+  }
+
+  return waited
+    .tapCatch(stopServer)
+    .then(runTests)
+    .finally(stopServer)
 }
 
 module.exports = startAndTest
