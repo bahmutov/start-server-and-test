6423340af2d7ae014737e2d5bcc2728773402fa0
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-09-13 11:19:11 -0400

    chore: add debug to bin script

--- src/bin/start.js
+++ src/bin/start.js
@@ -1,10 +1,15 @@
 #!/usr/bin/env node
 
+const debug = require('debug')('start-server-and-test')
+
 const args = process.argv.slice(2)
 const startAndTest = require('..')
 const utils = require('../utils')
 
-const { start, test, url } = utils.getArguments(args)
+debug('parsing CLI arguments: %o', args)
+const parsed = utils.getArguments(args)
+debug('parsed args: %o', parsed)
+const { start, test, url } = parsed
 
 console.log('starting server using command "%s"', start)
 console.log('and when url "%s" is responding', url)
