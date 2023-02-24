0e9984eb22fa9b4ac4132df63a0c1f072471fc66
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-02-24 08:39:03 -0500

    confirm ::1 address works (#355)
    * confirm ::1 address works
    
    * name of the script

---
+++ test/ip6.mjs
@@ -0,0 +1,17 @@
+import http from 'node:http';
+
+// Create a local server to receive data from
+const server = http.createServer();
+
+// Listen to the request event
+server.on('request', (request, res) => {
+  console.log('server responding')
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({
+    data: 'Hello World!',
+  }));
+});
+
+server.listen(8000, '::1', () => {
+  console.log('server is listening on ::1:8000')
+});
