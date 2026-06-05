c86b302bd3e84d06cab09d4b16e9a97cf6d1e84d
XhmikosR <xhmikosr@gmail.com>
2026-06-05 16:03:52 +0300

    chore: switch to fetch

--- test/client.js
+++ test/client.js
@@ -1,31 +1,26 @@
 // little test client to ping server at a given port
 const argv = require('minimist')(process.argv.slice(2), {
   alias: {
-    port: 'p'
-  }
+    port: 'p',
+  },
 })
 const port = argv.port || 9000
-const http = require('node:http')
 const url = `http://localhost:${port}`
-http
-  .get(url, { timeout: 10_000 }, (res) => {
-    res.resume()
 
-    if (res.statusCode < 200 || res.statusCode >= 300) {
+fetch(url, { signal: AbortSignal.timeout(10_000) })
+  .then((res) => {
+    if (!res.ok) {
       console.error(
         'url %s responded with status %d',
         url,
-        res.statusCode,
+        res.status,
       )
       process.exit(1)
     }
-
     console.log('url %s has responded 👍', url)
+    process.exit(0)
   })
-  .on('timeout', function () {
-    this.destroy(new Error(`timed out waiting for ${url}`))
-  })
-  .on('error', e => {
+  .catch((e) => {
     console.error(e.message)
     process.exit(1)
   })
