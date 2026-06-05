715a6933c7a583c7f6a8279cc0f4acd647ea40f3
XhmikosR <xhmikosr@gmail.com>
2026-06-05 10:55:51 +0300

    Address review comments

--- test/client.js
+++ test/client.js
@@ -8,8 +8,22 @@ const port = argv.port || 9000
 const http = require('node:http')
 const url = `http://localhost:${port}`
 http
-  .get(url, () => {
+  .get(url, { timeout: 10_000 }, (res) => {
+    res.resume()
+
+    if (res.statusCode < 200 || res.statusCode >= 300) {
+      console.error(
+        'url %s responded with status %d',
+        url,
+        res.statusCode,
+      )
+      process.exit(1)
+    }
+
     console.log('url %s has responded 👍', url)
+  })
+  .on('timeout', function () {
+    this.destroy(new Error(`timed out waiting for ${url}`))
   })
   .on('error', e => {
     console.error(e.message)
